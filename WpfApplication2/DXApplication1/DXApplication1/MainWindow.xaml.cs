5bc7e8d8af57ced3733a44c978ca2a138c2d9235
wang9174 <775242150@qq.com>
2017-07-25 17:53:31 +0800

    完成了窗口弹出BUG的修改

--- WpfApplication2/DXApplication1/DXApplication1/MainWindow.xaml.cs
+++ WpfApplication2/DXApplication1/DXApplication1/MainWindow.xaml.cs
@@ -25,15 +25,17 @@ namespace DXApplication1
     {
         public MainWindow()
         {
+            InitializeComponent();
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("zh-Hans");
+            //BarButtonItem barButtonItem1 = new BarButtonItem();
             barButtonItem1.ItemClick += BarButtonItem1_ItemClick;
-
+            //为什么注册监听程序会报错，我好气啊
         }
 
         private void BarButtonItem1_ItemClick(object sender, ItemClickEventArgs e)
         {
-            //Window1 win1 = new Window1();
-            //win1.Show();
+            Window1 win1 = new Window1();
+            win1.Show();
         }
 
 
